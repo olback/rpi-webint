310b73f1d03775d9c62c5aa334c923536e4908f0
Edwin <git@olback.net>
2017-08-05 15:30:51 +0200

    cpu.php

--- server-sent/cpu.php
+++ server-sent/cpu.php
@@ -2,7 +2,7 @@
 header('Content-Type: text/event-stream');
 header('Cache-Control: no-cache');
 
-$cpu = "grep 'cpu ' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {print usage \"\"}'";
+$cpu = "top -bn1 | grep \"Cpu(s)\" | \sed \"s/.*, *\([0-9.]*\)%* id.*/\\1/\" | \awk '{print 100 - $1}'";
 $cpu = shell_exec($cpu);
 echo "data: $cpu \n\n";
 flush();
